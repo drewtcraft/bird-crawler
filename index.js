b9124a2b769ffb8f4f2397c492560781b88ba3c9
dilekbaykara <73802910+dilekbaykara@users.noreply.github.com>
2023-06-06 10:05:16 -0400

    changed error on line 13, from goToEBirdLandingPage to goToEBirdHomePage and added missing argument within function (#1)

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const puppeteer = require("puppeteer");
 const EBird = require("./ebird");
 
 async function main() {
-    console.log(process.argv[2]);
+  console.log(process.argv[2]);
 
-    // get the browser and page object (puppeteer stuff)
-    // -- browser is only used to get the page
-    // -- page is used to interact with the page, select elements, etc.
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
+  // get the browser and page object (puppeteer stuff)
+  // -- browser is only used to get the page
+  // -- page is used to interact with the page, select elements, etc.
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
-    await EBird.goToEBirdLandingPage();
-    await EBird.logInToEBird(page);
-    await EBird.goToMyEBirdPage(page);
+  await EBird.goToEBirdHomePage(page);
+  await EBird.logInToEBird(page);
+  await EBird.goToMyEBirdPage(page);
 
-    await browser.close();
+  await browser.close();
 }
 
 main();
