c0f2d07e3bfc0f506b345fb16afd283646c8c1ce
Dilek Baykara <baykara.d@gmail.com>
2023-06-19 20:47:07 -0400

    added a function to get title of checklist

--- ebird.ts
+++ ebird.ts
@@ -6,48 +6,65 @@ const E_BIRD_USERNAME = "drewtcraft";
 const E_BIRD_PASSWORD = "MyD0GisCOOL!";
 
 export class EBird {
-
   static async goToEBirdHomePage(page: Page) {
     // this pattern will be used throughout; we use Promise.all
     // to navigate or click somewhere, and waitForNavigation to
     // ensure the browser has loaded the next page
     await Promise.all([
-        page.goto(E_BIRD_LANDING), // go to eBird landing page
-        page.waitForNavigation(),
+      page.goto(E_BIRD_LANDING), // go to eBird landing page
+      page.waitForNavigation(),
     ]);
   }
 
   // class that is basically just a namespace for static methods
   // relating to eBird login
   static async logInToEBird(page: Page) {
-      // the HTML selector that gets the sign up button
-      const signUpSelector = ".HeaderEbird-menu-tools ul li:nth-child(2)";
-      await page.waitForSelector(signUpSelector);
-      await page.click(signUpSelector);
+    // the HTML selector that gets the sign up button
+    const signUpSelector = ".HeaderEbird-menu-tools ul li:nth-child(2)";
+    await page.waitForSelector(signUpSelector);
+    await page.click(signUpSelector);
 
-      // select the username input and type in the username
-      const usernameInputSelector = "#input-user-name";
-      await page.waitForSelector(usernameInputSelector);
-      await page.type(usernameInputSelector, E_BIRD_USERNAME);
+    // select the username input and type in the username
+    const usernameInputSelector = "#input-user-name";
+    await page.waitForSelector(usernameInputSelector);
+    await page.type(usernameInputSelector, E_BIRD_USERNAME);
 
-      // select the password input and type in the password
-      const passwordInputSelector = "#input-password";
-      await page.waitForSelector(passwordInputSelector);
-      await page.type(passwordInputSelector, E_BIRD_PASSWORD);
+    // select the password input and type in the password
+    const passwordInputSelector = "#input-password";
+    await page.waitForSelector(passwordInputSelector);
+    await page.type(passwordInputSelector, E_BIRD_PASSWORD);
 
-      // submit the login form
-      await Promise.all([
-          page.click("#form-submit"),
-          page.waitForNavigation(),
-      ]);
+    // submit the login form
+    await Promise.all([page.click("#form-submit"), page.waitForNavigation()]);
   }
 
   static async goToMyEBirdPage(page: Page) {
-      const selector = "a.HeaderEbird-link[href='/myebird']";
-      await page.waitForSelector(selector);
-      await Promise.all([
-          page.click(selector),
-          page.waitForNavigation(),
-      ]);
+    const selector = "a.HeaderEbird-link[href='/myebird']";
+    await page.waitForSelector(selector);
+    await Promise.all([page.click(selector), page.waitForNavigation()]);
+  }
+
+  static async goToMyCheckLists(page: Page) {
+    await page.goto("https://ebird.org/mychecklists");
+  }
+
+  static async getMyListLinks(page: Page) {
+    const selector = "div.ResultsStats-title > h3 > a";
+    const hrefs = await page.$$eval(selector, (list) =>
+      list.map((elm) => elm.href)
+    );
+    return hrefs;
+  }
+
+  static async goToMyLinkPage(page: Page, href: string) {
+    await page.goto(href);
+  }
+
+  static async getMyListTitle(page: Page) {
+    const titleSelector = "div.SectionHeading.u-margin-none>div>time";
+    let element = await page.$(titleSelector);
+    let value = await page.evaluate((title) => title?.textContent, element);
+    // await page.waitForSelector(title);
+    return value?.replace(/\t/g, "");
   }
 }
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import puppeteer from "puppeteer";
-import {EBird} from "./ebird";
+import { EBird } from "./ebird";
 
-async function delay(time: number){
-await new Promise(resolve => setTimeout(resolve, time))
+async function delay(time: number) {
+  await new Promise((resolve) => setTimeout(resolve, time));
 }
 
 async function main() {
@@ -15,11 +15,23 @@ async function main() {
   const page = await browser.newPage();
 
   await EBird.goToEBirdHomePage(page);
-  await delay(10000)
+  await delay(1000);
   await EBird.logInToEBird(page);
-  await delay(10000)
+  await delay(1000);
   await EBird.goToMyEBirdPage(page);
-  await delay(10000)
+  await delay(1000);
+  await EBird.goToMyCheckLists(page);
+  await delay(1000);
+  const hrefs = await EBird.getMyListLinks(page);
+  console.log(hrefs);
+  for (const href of hrefs) {
+    console.log(href);
+    await EBird.goToMyLinkPage(page, href);
+    await delay(1000);
+    const title = await EBird.getMyListTitle(page);
+    console.log(title);
+    await delay(1000);
+  }
 
   await browser.close();
 }
